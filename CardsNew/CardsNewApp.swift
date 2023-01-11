b9a455f2be3df4575de2b001658388498537484e
LinaAlmusfir <LinaAlmusfir@gmail.com>
2023-01-11 16:33:38 +0300

    Initial Commit

---
+++ CardsNew/CardsNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsNewApp.swift
+//  CardsNew
+//
+//  Created by Lina on 18/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CardsNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
